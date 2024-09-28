394eaa4855f67f5adccc4b727716b46d8c8f95d6
yael tuvia <yael159753@gmail.com>
2024-09-28 23:22:47 +0300

    header + banner

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./Components/Header/Header";
+import Banner from "./Components/Banner/Banner";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+        <Banner />
+    </div>
+  );
+}
+
+export default App;
